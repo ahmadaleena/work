556c3e746505413d98b75d650f18f9b38ecc5030
ahmadaleena <aleena.ahmad@algo.com>
2024-01-11 21:50:05 +0500

    main.py - parsed HTML, added pandas for analyzing

---
+++ class.py
@@ -0,0 +1,14 @@
+#class StockData
+#dataFrame: MarketWatch data
+#dataFrame: indexes 
+#tabletoDF -> given a table, creates a dataFrame
+#greatestChangeList -> from dataFrame
+#highestIndexMonth
+#highestIndexDay
+#constant variable: url
+
+#class HTMLParser:
+#getPage -> given a URL, gets chromeDriver to open it and returns HTML tree object returned by BeautifulSoup
+#
+#
+# 
