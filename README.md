db4c32c7c238eac17cc545f47f68ef1b68d231b5
ahmadaleena <aleena.ahmad@algo.com>
2024-01-15 17:10:57 +0500

    Set up FastAPI and requirments.txt

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-## Task 1 
+## Task 5
 
-You have a list of n items. 
- 
-e.g = [1 , 1, 4, 4, 6, 32, 32, 32, 5, 522, 0, .....] , [24 , 3, 5, 5, 6, 11, 11, 32, , 5 , 5, 52, 32, 0, -4, .....]
- 
-You need to find out the occurrence of each value from this list. 
-
-Related Readings/Materials: 
-* PEP8: https://peps.python.org/pep-0008/ 
-* Prompt Engineering: https://youtu.be/mBYu5NoXBcs?si=mYE5zgwATnRSDQk0
+Set up a server using FastAPI.
--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-def find_occurrences(numbers):
+from typing import Union
+from fastapi import FastAPI 
 
-    #Creating a dictionary to store number and its occurrences as key value pair:
-    num_count_pairs = {} 
-    for number in numbers:
-        if (number in num_count_pairs):  #Number exists in dict already
-            num_count_pairs[number] += 1
-        else:    #First occurrence of number
-            num_count_pairs[number] = 1
-    print("Occurrences of each number in list: ", num_count_pairs)
+app = FastAPI()
 
-#Test cases
-example1 = [
-    1 , 1, 4, 
-    4, 6, 32, 
-    32, 32, 5, 
-    522, 0,
-    ]
-example2 = [
-    24 , 3, 5, 
-    5, 6, 11, 
-    11, 32, 5, 
-    5, 52, 32, 
-    0, -4,
-    ]
-example3 = []   
-find_occurrences(example1)
-find_occurrences(example2)
-find_occurrences(example3)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-
- 
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
